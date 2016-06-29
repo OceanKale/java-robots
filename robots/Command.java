2291f788c0c0628580a66dca3c220de562bd777b
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-06-29 00:56:43 -0500

    Add files via upload

---
+++ robots/Command.java
@@ -0,0 +1,60 @@
+package robots.commands;
+
+/**
+   The abstract class of all commands.<br><br>
+   
+   The player controls the robot by issuing commands.<br><br>
+
+   * The Move command moves the robot to an adjacent square, in one of the four directions 
+   north, east, south or west.<br><br>
+   * The Pick command is used to pick packages. Packages are initially available from home bases. 
+   Packages may not be picked up if they are too heavy, or if there are no packages available.<br><br>
+   * The Drop command is used to drop packages. Packages are always dropped. <br><br>
+
+   The Pick and Drop commands are followed by a sequence of zero or more package ids. It is not an 
+   error to attempt to pick up packages that are not available. Nor is it an error to drop packages 
+   that are not being carried.<br>
+
+   The robot dies if<br>
+
+   1. it moves (or is pushed) onto a lethal tile, i.e., one containing water, or<br>
+   2. it sends a malformed command to the server, or<br>
+   3. it spends all its money. <br><br>
+
+   In an effort to minimize the chances of the robot dying due to (2) above, 
+   this class ensures that the bid is always non-zero. 
+*/
+public abstract class Command {
+
+    private int bid;
+
+    /**
+       Creates a command with bid 1. 
+    */
+    public Command() {
+	bid = 1;
+    }
+
+    /**
+       Creates a command with the given bid value. 
+    */
+    public Command(int bid) {
+	//if (bid != 0) {
+	this.bid = bid;
+	//}
+    }
+
+    /**
+       Returns the bid associated with the command, unless the bid is zero, in which case 
+       the value -1 is returned. 
+    */
+    public int getBid() {
+	if (bid != 0) {
+	    return bid;
+	}
+	else {
+	    return -1;
+	}
+    }
+
+}
---
+++ robots/DropCommand.java
@@ -0,0 +1,84 @@
+package robots.commands;
+
+import robots.world.*;
+import robots.world.Package;
+import java.util.Iterator;
+
+/**
+  
+
+   A class that models a drop command.
+*/
+public class DropCommand extends Command {
+
+    private PackageCollection packages;
+    private Package pkg;
+
+    /**
+       Constructs a drop command with an empty collection of packages and a bid of 1. 
+    */
+    public DropCommand() {
+	super();
+	packages = new PackageCollection();
+    }
+
+    /**
+       Constructs a drop command with the given bid and an empty collection of packages. 
+    */
+    public DropCommand(int bid) {
+	super(bid);
+	packages = new PackageCollection();
+    }
+
+    /**
+       Construct a drop command with the given collection of packages and a bid of 1. 
+    */
+    public DropCommand(PackageCollection packages) {
+	super();
+	this.packages = packages;
+    }
+
+    /**
+       Construct a drop command with the given bid and the given collection of packages.
+    */
+    public DropCommand(int bid, PackageCollection packages) {
+	super(bid);
+	this.packages = packages;
+    }
+
+    /**
+       Construct a drop command with the given package and a bid of 1. 
+    */
+    public DropCommand(Package pkg) {
+	super();
+	this.pkg = pkg;
+    }
+    
+    /**
+       Construct a drop command with the given bid and the given package. 
+    */
+    public DropCommand(int bid, Package pkg) {
+	super(bid);
+	this.pkg = pkg;
+    }
+
+    /**
+       Returns a string representation of the drop command in the form expected by the server. 
+    */
+    public String toString() {
+	String s = "";
+	s = s + this.getBid() + " Drop";
+	if (this.packages != null) {
+	    Iterator<Package> it = packages.iterator();
+	    while(it.hasNext()) {
+		s = s + " " + it.next().getId();
+	    }
+	}
+	else if (this.pkg != null) {
+	    s = s + " " + pkg.getId();
+	}
+       
+	return s;
+    }
+    
+}
