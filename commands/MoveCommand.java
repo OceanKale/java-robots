1c329138aee420c1dd863fa589fa0b9b9352dbc6
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-07-01 20:36:31 -0500

    Add files via upload

---
+++ commands/MoveCommand.java
@@ -0,0 +1,35 @@
+package robots.commands;
+
+import robots.world.*;
+
+/**
+    
+   This class models a move command.
+*/
+public class MoveCommand extends Command {
+
+    private Direction dir;
+
+    /**
+       Constructs a move command, in the given direction, with bid 1.
+    */
+    public MoveCommand(Direction dir) {
+	super();
+	this.dir = dir;
+    }
+    
+    /**
+       Constructs a move command with the given bid and direction.
+    */
+    public MoveCommand(int bid, Direction dir) {
+	super(bid);
+	this.dir = dir;
+    };
+    
+    /**
+       Returns a string representation of the move command in the form expected by the server. 
+    */
+    public String toString() {
+	return this.getBid() + " Move " + this.dir.toString(); 
+    }
+}
