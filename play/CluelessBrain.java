03ae66e38e963c03bbc8e068560ab796770763a6
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-07-24 12:54:20 -0500

    Update CluelessBrain.java

--- play/CluelessBrain.java
+++ play/CluelessBrain.java
@@ -5,19 +5,19 @@ import robots.world.*;
 import robots.commands.*;
 
 /**
-   Class CluelessBrain does nothing.
+   Class CluelessBrain has no real objective
 */
 public class CluelessBrain extends Brain {
 
     /**
-       Creates a clueless brain for use with the given game. 
+       Creates a clueless brain for use with the given game
     */
     public CluelessBrain(Game game) {
         super(game);
     }
 
     /**
-       Stay where you are and do nothing.
+       Stay where you are and do nothing, sending drop commands when we have no object to drop
     */
     public Command next() {
 	return new DropCommand();
