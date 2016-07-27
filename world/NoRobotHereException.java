ef02ec6a81d8280e58690b4824c4d86f540ad824
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-07-26 19:21:06 -0500

    Add files via upload

---
+++ world/NoRobotHereException.java
@@ -0,0 +1,16 @@
+package robots.world;
+
+/**
+  
+   A NoRobotHereException is thrown by Tile.getRobot() to indicate
+   that the invoking tile is not occupied by a robot.
+*/
+public class NoRobotHereException extends RuntimeException {
+
+  /**
+     Constructs a NoRobotHereException
+  */
+  public NoRobotHereException() {
+    super("No robot on this tile.");
+  }
+}
